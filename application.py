62f47242c0634a89ccd6ed7ac23a7fb67df3c5bf
Akash0811 <aakashbiswal@gmail.com>
2018-09-20 21:09:36 +0530

    Incomplete and confused

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from flask import Flask , render_template
 from flask_socketio import SocketIO, emit
@@ -7,7 +8,9 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-channels = []
+channels = {}
+
+channel_title_global = ''
 
 def apology(message, code=400):
     """Render message as an apology to user."""
@@ -15,14 +18,39 @@ def apology(message, code=400):
 
 @app.route("/")
 def index():
-    return render_template("index.html" , channels = channels)
+    return render_template("index.html" ,chan = channel_title_global , channels = channels)
 
 @socketio.on("submit channel")
 def channel(data):
-    name = data["channel_name"]
-    for channel in channels:
-        if channel["title"] == name:
-            return apology("Name already assigned to other channel" , 400)
-    channels.append({ "title": name , "messages": {} })
-    rest = channels[len(channels)-1:]
+    title = data["channel_name"]
+    if title in channels:
+        return apology("Name already assigned to other channel",400)
+    channels.update( { title : [] })
+    rest = { "title": title }
     emit("channel list", rest , broadcast=True)
+
+@socketio.on("submit message")
+def message(data):
+    channel_title_global = data["channel_title"]
+    channel_title = data["channel_title"]
+    message = data["message"]
+    name = data["name"]
+    #print(message)
+    timestamp = datetime.datetime.now()
+    string = f"{name}"+" :: "f"{timestamp}" + " :: " + f"{message}"
+    prev_messages = channels[channel_title]
+    prev_messages.append( string )
+    channels[channel_title] = prev_messages
+    rest = { "title": prev_messages }
+    #print(channels)
+    emit("message list", rest , broadcast=True)
+
+@socketio.on("load channel")
+def load(data):
+    #print(channels)
+    channel_title_global = data["channel_title"]
+    channel_title = data["channel_title"]
+    prev_messages = channels[channel_title]
+    rest = { "title": prev_messages }
+    #print(rest["title"])
+    emit("load list", rest , broadcast=True)
---
+++ test.py
@@ -0,0 +1,8 @@
+a = { "x": []  , "y" : 5}
+
+string = "x"
+a[string].append(24)
+x = a[string]
+
+for key in a:
+    print(a[key])
